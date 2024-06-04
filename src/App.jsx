dd852e58d7328da6c8e80470f5a6bcf73fb88553
Tanim <omoraltanim@gmail.com>
2024-06-04 12:40:35 +0600

    Basic Router implementation in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,23 @@ import HomeCards from './components/HomeCards'
 import JobListings from './components/JobListings'
 import ViewAllJobs from './components/ViewAllJobs'
 
-const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero/>
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
+import { 
+  Route, 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  RouterProvider 
+} from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route index element={ <h1>My App</h1> } />
+    // <Route path='/about' element={ <h1>My App</h1> } />
+    
   )
+)
+
+const App = () => {
+  return <RouterProvider router={router}/>
 }
 
 export default App

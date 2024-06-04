6465c2ecd5526764ede59b1ebc3d81bb34d5be44
Tanim <omoraltanim@gmail.com>
2024-06-04 07:07:53 +0600

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,18 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
+import JobListings from './components/JobListings'
+import ViewAllJobs from './components/ViewAllJobs'
 
 const App = () => {
-  const bigtxt = "text-5xl";
-  const name = "Tanim"
-  const loggedIn = false;
-  const styles = {
-    color: 'red',
-    fontSize: '55px'
-  }
-
-  const names = ['Tanim', 'Omor', 'Batash', 'Tuki']
-
   return (
     <>
-      <div className={bigtxt}>Hello {name}</div>
-      <p style={{ color: 'red', fontSize: '24px'}}>Red text</p>
-      <p style={styles}>Style using obj</p>
-    <ul>
-      {
-        names.map((name, index) => <li key={index}>{name}</li>)
-      }
-      {loggedIn ? <h1>Hello Member</h1> : <h1>Hello Guest</h1>} // 
-      {loggedIn && <h1>Hello Member</h1>}
-    </ul>
+      <Navbar />
+      <Hero/>
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
     </>
   )
 }
---
+++ src/AppPrac.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const App = () => {
+  const bigtxt = "text-5xl";
+  const name = "Tanim"
+  const loggedIn = false;
+  const styles = {
+    color: 'red',
+    fontSize: '55px'
+  }
+
+  const names = ['Tanim', 'Omor', 'Batash', 'Tuki']
+
+  return (
+    <>
+      <div className={bigtxt}>Hello {name}</div>
+      
+      <p style={{ color: 'red', fontSize: '24px'}}>Red text</p>
+      
+      <p style={styles}>
+        Style using obj
+      </p>
+
+    <ul>
+      {
+        names.map((name, index) => <li key={index}>{name}</li>)
+      }
+      {loggedIn ? <h1>Hello Member</h1> : <h1>Hello Guest</h1>} // 
+      {loggedIn && <h1>Hello Member</h1>}
+    </ul>
+    </>
+  )
+}
+
+export default App

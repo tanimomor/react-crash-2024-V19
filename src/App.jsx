0871d03980728f439cc6a05c90d4cbc98dbb420a
Tanim <omoraltanim@gmail.com>
2024-06-08 08:22:43 +0600

    Jobpage with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ import HomePage from "./pages/HomePage.jsx";
 import MainLayout from "./layouts/MainLayout.jsx";
 import JobsPage from "./pages/JobsPage.jsx";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
+import JobPage from "./pages/JobPage.jsx";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<MainLayout/>}>
             <Route index element={<HomePage/>}/>
             <Route path='/jobs' element={<JobsPage />}/>
+            <Route path='/jobs/:id' element={<JobPage />}/>
             <Route path='*' element={<NotFoundPage />}/>
         </Route>
     )
---
+++ src/pages/JobPage.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner.jsx";
+
+const JobPage = () => {
+    const { id } = useParams();
+    const [job, setJob] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchJob = async () =>{
+            try {
+                const res = await fetch(`/api/jobs/${id}`);
+                const data = await res.json();
+                setJob(data)
+            } catch (error) {
+                console.log('Error fetching data', error);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchJob()
+    }, []);
+
+    return loading? <Spinner /> : <div>{job.title}</div>
+}
+
+export default JobPage;

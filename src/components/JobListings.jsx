330bc5ef0c331d4dad37d8cedfb36510113608c3
Tanim <omoraltanim@gmail.com>
2024-06-08 06:18:51 +0600

    Proxy for api and spinner while loading

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,15 +1,16 @@
 import {useState, useEffect} from "react";
 import JobListing from './JobListing';
+import Spinner from './Spinner';
 
 const JobListings = ({isHome = false}) => {
     const [jobs, setJobs] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-
         const fetchJobs = async () => {
+            const apiUrl = isHome ? 'api/jobs?_limit=3' : 'api/jobs';
             try {
-                const res = await fetch('http://localhost:8000/jobs');
+                const res = await fetch(apiUrl);
                 const data = await res.json();
                 setJobs(data)
             } catch (error) {
@@ -29,13 +30,15 @@ const JobListings = ({isHome = false}) => {
                     <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
                         {isHome ? 'Recent jobs' : 'Browse Jobs'}
                     </h2>
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                        { loading ? <h2>Loading..</h2> : (
-                            jobs.map((job) => (
-                                <JobListing key={job.id} job={job}/>
-                            ))
+
+                        { loading ? <Spinner loading={loading} /> : (
+                            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                                {jobs.map((job) => (
+                                    <JobListing key={job.id} job={job}/>
+                                ))}
+                            </div>
                         )}
-                    </div>
+
                 </div>
             </section>
         </>
